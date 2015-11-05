842cf6c5c145a459214a30f46e2959e3f015e0e7
kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2015-11-05 10:01:22 +0100

    Add proper csv output

--- src/exploration_test_node.py
+++ src/exploration_test_node.py
@@ -2,6 +2,10 @@
 import roslib; roslib.load_manifest('exploration_evaluation')
 import math
 import rospy
+
+import csv
+from time import gmtime, strftime
+
 #import sensor_msgs.msg
 #import actionlib
 #from control_msgs.msg import *
@@ -20,6 +24,7 @@ from hector_move_base_msgs.msg import MoveBaseActionGoal
 from geometry_msgs.msg import Twist
 from hector_worldmodel_msgs.srv import AddObject
 from hector_worldmodel_msgs.msg import ObjectState
+from nav_msgs.msg import OccupancyGrid
 
 from sar_msgs.msg import VictimAnswer, Victims
 
@@ -27,6 +32,18 @@ from sar_msgs.msg import VictimAnswer, Victims
 class ExplorationTester:
 
     def __init__(self):
+      
+      self._occupancy_grid = None
+      self._num_victims_found = 0
+      
+      self._date_string = strftime("%Y_%m_%d_%H-%M-%S", gmtime())
+      
+      self._map_file = open(self._date_string + '_map_data.csv', 'wb')
+      self._victim_file = open(self._date_string + '_victim_data.csv', 'wb')
+      
+      self._map_file_writer = csv.writer(self._map_file)
+      self._victim_file_writer = csv.writer(self._victim_file)
+      
       
       rospy.loginfo("Init")
       
@@ -38,10 +55,40 @@ class ExplorationTester:
       rospy.sleep(0.2)
             
       self._subscriber = rospy.Subscriber("victimFound", Victims, self._on_victim_found)
+      self._map_sub = rospy.Subscriber('scanmatcher_map', OccupancyGrid, self.handle_occupancy_grid)
       
       rospy.sleep(0.2)
       
       self.send_autonomy_start()
+      
+    def add_map_data_entry(self):           
+      
+      num_cells = self.computeKnownCellsEnv()
+            
+      time_seconds = str((rospy.Time.now() - self._autonomy_start_time).to_sec())
+      
+      self._map_file_writer.writerow([str(time_seconds), str(num_cells)])
+      
+    def computeKnownCellsEnv(self):
+        if not self.occupancy_grid:
+            return 0
+        data = self.occupancy_grid.data
+        nbOfUnknownCells = 0.0
+        for eachCells in data:
+            if eachCells == -1:
+                nbOfUnknownCells += 1
+        #self.pourcentageOfKnownEnv = 1.0 - (nbOfUnknownCells / len(data))
+        
+        return len(data) - nbOfUnknownCells
+   
+    def map_timer_callback(self, event):
+        self.add_map_data_entry()
+   
+    def handle_occupancy_grid(self, msg):
+        self.occupancy_grid = msg  
+      
+
+   
       
     def _confirm_victim(self):
         self._victimAnswer = VictimAnswer.CONFIRM
@@ -68,6 +115,7 @@ class ExplorationTester:
             #subprocess.call(['/usr/bin/canberra-gtk-play','--id','phone-incoming-call']) 
             
             victim_found_time = rospy.Time.now()
+            self._num_victims_found += 1 
             
             status_msg = "Time " + str((victim_found_time - self._autonomy_start_time).to_sec())
             status_msg += " received victim found message, id = "
@@ -78,6 +126,9 @@ class ExplorationTester:
             self._task_id = message.victim_ids[0]
             
             self._confirm_victim()
+            
+            self._victim_file_writer.writerow([str((victim_found_time - self._autonomy_start_time).to_sec()), self._num_victims_found])
+            
             #self._widget.confirm_push_button.setEnabled(True)
             #self._widget.confirm_push_button.setStyleSheet('QPushButton:enabled {background-color: lightgreen; border-style: outset; border-width: 2px; border-radius: 5px; border-color: black; padding: 6px;}')
             #self._widget.get_closer_push_button.setEnabled(True)
@@ -154,8 +205,12 @@ class ExplorationTester:
                 rospy.loginfo("Sent start request")
                 self._autonomy_start_time = rospy.Time.now()
                 
+                self.add_map_data_entry()
+                
                 self._autonomyPublisher.publish(True)
                 self._autonomous = True
+                
+                self._map_timer = rospy.Timer(rospy.Duration(10), self.map_timer_callback)
                 #self._widget.autonomy_button.setText(self._stopAutonomyText)
                 #self._write_message('started autonomy')
             
@@ -174,11 +229,11 @@ class ExplorationTester:
             self._trajectory_points = []
         self._client.cancel_all_goals()
 
-    def run(self):
+    #def run(self):
         #next_start_time = rospy.Time.now()
         
-        while not rospy.is_shutdown():
-          rospy.sleep(0.1)
+     #   while not rospy.is_shutdown():
+     #     rospy.sleep(0.1)
           
 if __name__ == "__main__":
         rospy.init_node("exploration_evaluation")
@@ -187,4 +242,13 @@ if __name__ == "__main__":
         except rospy.exceptions.ROSInterruptException:
                 pass
         exploration_tester = ExplorationTester()
-        rospy.spin()
+        
+        #rospy.spin()
+        
+        r = rospy.Rate(10) # 10hz
+        
+        while not (rospy.Time.now() - exploration_tester._autonomy_start_time).to_sec() > 60 * 10:
+          r.sleep()
+          
+        rospy.loginfo("Experiment at " + exploration_tester._date_string + " ended.")
+        rospy.loginfo("Robot found " + str(exploration_tester._num_victims_found) + " victims.")
