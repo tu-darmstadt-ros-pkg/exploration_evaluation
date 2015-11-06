bbc0c36b35fd12dc667a2597c2bf73bdcc6df424
kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2015-11-06 09:05:17 +0100

    Kill and restart master on every iteration to avoid potential hiccups

--- scripts/run_looped.sh
+++ scripts/run_looped.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 
-COUNTER=0
+COUNTER=1
 
 while :
 do
-        optirun roslaunch exploration_evaluation launch_eval_complete.launch
-        echo Ran $COUNTER experiments
+        roscore&
         sleep 10
+        #optirun roslaunch exploration_evaluation launch_eval_complete.launch
+        echo Ran $COUNTER experiments
+        ((COUNTER+=1))
+        sleep 10
+        pkill roscore
         pkill gazebo
         pkill apport
         sleep 10
