8d91d1641a1b377cbb4e0642632dedaedeb2f6a0
Stefan Wegener <stefan@stefan-wegener.eu>
2016-08-04 12:22:52 +0200

    Fixed error in launchfile and startup-script

--- scripts/run_looped.sh
+++ scripts/run_looped.sh
@@ -10,13 +10,12 @@ if [ ! -z "$1" ]; then
     do
         roscore&
         sleep 10
-        optirun roslaunch exploration_evaluation launch_eval_complete.launch
+        roslaunch exploration_evaluation launch_eval_complete.launch
         echo Ran $COUNTER experiments
         ((COUNTER+=1))
         sleep 10
         pkill roscore
         pkill gazebo
-        pkill apport
         sleep 10
     done
 
@@ -26,13 +25,12 @@ else
     do
         roscore&
         sleep 10
-        optirun roslaunch exploration_evaluation launch_eval_complete.launch
+        roslaunch exploration_evaluation launch_eval_complete.launch
         echo Ran $COUNTER experiments
         ((COUNTER+=1))
         sleep 10
         pkill roscore
         pkill gazebo
-        pkill apport
         sleep 10
     done
 fi
