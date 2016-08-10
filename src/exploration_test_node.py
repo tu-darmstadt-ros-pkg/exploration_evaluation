77f7f8baba3f258b2fa0d10e2c2dd93e46f4a2cb
Stefan Wegener <stefan@stefan-wegener.eu>
2016-08-10 13:03:06 +0200

    Final script refactoring.

--- src/exploration_test_node.py
+++ src/exploration_test_node.py
@@ -1,242 +1,171 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('exploration_evaluation')
-import math
-import rospy
-
 import csv
 from time import gmtime, strftime
-
-#import sensor_msgs.msg
-#import actionlib
-#from control_msgs.msg import *
-#from trajectory_msgs.msg import *
 from std_msgs.msg import *
-from copy import deepcopy
-
-
-from task_msgs.msg import TasksModel
-from std_msgs.msg import Bool, String, Float32, Empty
-from task_msgs.msg import SupervisionCommand, TaskState, TaskCost, SupervisionSettings
-from task_msgs.srv import AddTask
-from sar_msgs.msg import SarTaskTypes
-from monstertruck_msgs.msg import Status
-from hector_move_base_msgs.msg import MoveBaseActionGoal
-from geometry_msgs.msg import Twist
-from hector_worldmodel_msgs.srv import AddObject
-from hector_worldmodel_msgs.msg import ObjectState
+from std_msgs.msg import Bool, String
 from nav_msgs.msg import OccupancyGrid
-
 from sar_msgs.msg import VictimAnswer, Victims
+import rospy
+import roslib; roslib.load_manifest('exploration_evaluation')
 
 
-class ExplorationTester:
+class ExplorationTester(object):
+
+    '''Class that automatically starts a exploration behaviour on given world and
+    creates reports abouts victims found and the created map.
+    '''
 
     def __init__(self):
-      
-      self._occupancy_grid = None
-      self._num_victims_found = 0
-      
-      self._date_string = strftime("%Y_%m_%d_%H-%M-%S", gmtime())
-      
-      self._map_file = open(self._date_string + '_map_data.csv', 'wb')
-      self._victim_file = open(self._date_string + '_victim_data.csv', 'wb')
-      
-      self._map_file_writer = csv.writer(self._map_file)
-      self._victim_file_writer = csv.writer(self._victim_file)
-      
-      
-      rospy.loginfo("Init")
-      
-      self._autonomy_start_time = rospy.Time.now()
-      
-      self._publisher = rospy.Publisher("victimAnswer", VictimAnswer, queue_size=5)
-      
-      self._sys_command_publisher = rospy.Publisher("syscommand", String, queue_size=5)
-      self._autonomyPublisher = rospy.Publisher('taskallocation/startAutonomy', Bool, queue_size=5)
-      
-      rospy.sleep(0.2)
-            
-      self._subscriber = rospy.Subscriber("victimFound", Victims, self._on_victim_found)
-      self._map_sub = rospy.Subscriber('scanmatcher_map', OccupancyGrid, self.handle_occupancy_grid)
-      
-      rospy.sleep(30)
-      
-      self.send_autonomy_start()
-      
-    def add_map_data_entry(self):           
-      
-      num_cells = self.computeKnownCellsEnv()
-            
-      time_seconds = str((rospy.Time.now() - self._autonomy_start_time).to_sec())
-      
-      self._map_file_writer.writerow([str(time_seconds), str(num_cells)])
-      
-    def computeKnownCellsEnv(self):
-        if not self.occupancy_grid:
-            return 0
-        data = self.occupancy_grid.data
-        nbOfUnknownCells = 0.0
-        for eachCells in data:
-            if eachCells == -1:
-                nbOfUnknownCells += 1
-        #self.pourcentageOfKnownEnv = 1.0 - (nbOfUnknownCells / len(data))
-        
-        return len(data) - nbOfUnknownCells
-   
-    def map_timer_callback(self, event):
-        self.add_map_data_entry()
-   
-    def handle_occupancy_grid(self, msg):
-        self.occupancy_grid = msg  
-      
+        '''Intializes variables, publishers, subscribers and files, 
+        then starts autonomy.
+        '''
+        self._occupancy_grid = None
+        self.num_victims_found = 0
+        self.autonomy_start_time = rospy.Time.now()
+        self._autonomous = False
+        self._map_timer = None
+        self.date_string = strftime("%Y_%m_%d_%H-%M-%S", gmtime())
 
-   
+        # File variables
+        self._map_file = None
+        self._map_file_writer = None
+        self._victim_file = None
+        self._victim_file_writer = None
+
+        # ROS Publishers & Subscribers
+        self._publisher = None
+        self.sys_command_publisher = None
+        self._autonomy_publisher = None
+        self._subscriber = None
+        self._map_sub = None
+
+        rospy.loginfo("Init ExplorationTester")
       
-    def _confirm_victim(self):
-        self._victimAnswer = VictimAnswer.CONFIRM
-        
-        #self._publish_answer()
-        
-        answer = VictimAnswer()
-        answer.task_id = self._task_id
-        answer.answer = self._victimAnswer
-        self._publisher.publish(answer)
-        
-        #self._widget.confirm_push_button.setEnabled(False)
-        #self._widget.get_closer_push_button.setEnabled(False)
-        #self._widget.ignore_push_button.setEnabled(False)
-        #self._widget.postpone_push_button.setEnabled(False)        
-        
-        
-        status_msg = "confirmed victim, id = "
-        status_msg += self._task_id
-        rospy.loginfo(status_msg)  
+        self.init_map_and_victim_files()
+
+        self.init_publishers()
+        rospy.sleep(0.2)    
+        self.init_subscribers()
+        rospy.sleep(30)
       
-    def _on_victim_found(self, message):
+        self.send_autonomy_start()
+
+
+    def init_map_and_victim_files(self):
+        '''Init file-writers.'''
+        self._map_file = open(self.date_string + '_map_data.csv', 'wb')
+        self._map_file_writer = csv.writer(self._map_file)
+
+        self._victim_file = open(self.date_string + '_victim_data.csv', 'wb')
+        self._victim_file_writer = csv.writer(self._victim_file)
+
+
+    def init_publishers(self):
+        '''Initializes publishers used during exploration.'''
+        self._publisher = rospy.Publisher("victimAnswer", VictimAnswer, queue_size=5)
+        self.sys_command_publisher = rospy.Publisher("syscommand", String, queue_size=5)
+        self._autonomy_publisher = rospy.Publisher('taskallocation/startAutonomy', Bool, queue_size=5)
+
+
+    def init_subscribers(self):
+        '''Initializes subscribers used during exploration.'''
+        self._subscriber = rospy.Subscriber("victimFound", Victims, self.handle_victim_found)
+        self._map_sub = rospy.Subscriber('scanmatcher_map', OccupancyGrid, self.handle_occupancy_grid)
+
+
+    def handle_victim_found(self, message):
+        '''Callback method for found victims'''
         if not len(message.victim_ids) == 0:
-            #subprocess.call(['/usr/bin/canberra-gtk-play','--id','phone-incoming-call']) 
-            
             victim_found_time = rospy.Time.now()
-            self._num_victims_found += 1 
+            self.num_victims_found += 1 
             
-            status_msg = "Time " + str((victim_found_time - self._autonomy_start_time).to_sec())
-            status_msg += " received victim found message, id = "
-            status_msg += message.victim_ids[0]
-            #self._print_message_signal.emit(status_msg)
-            rospy.loginfo(status_msg)
-            
-            self._task_id = message.victim_ids[0]
-            
-            self._confirm_victim()
-            
-            self._victim_file_writer.writerow([str((victim_found_time - self._autonomy_start_time).to_sec()), self._num_victims_found])
-            
-            #self._widget.confirm_push_button.setEnabled(True)
-            #self._widget.confirm_push_button.setStyleSheet('QPushButton:enabled {background-color: lightgreen; border-style: outset; border-width: 2px; border-radius: 5px; border-color: black; padding: 6px;}')
-            #self._widget.get_closer_push_button.setEnabled(True)
-            #self._widget.get_closer_push_button.setStyleSheet('QPushButton:enabled {background-color: yellow; border-style: outset; border-width: 2px; border-radius: 5px; border-color: black; padding: 6px;}')
-            #self._widget.ignore_push_button.setEnabled(True)
-            #self._widget.ignore_push_button.setStyleSheet('QPushButton:enabled {background-color: red; border-style: outset; border-width: 2px; border-radius: 5px; border-color: black; padding: 6px;}')
-            #self._widget.postpone_push_button.setEnabled(True)
-            
-        else:
-            #self._widget.confirm_push_button.setEnabled(False)
-            #self._widget.get_closer_push_button.setEnabled(False)
-            #self._widget.ignore_push_button.setEnabled(False)
-            #self._widget.postpone_push_button.setEnabled(False)    
-            pass
-      
+            self.log_new_victim(victim_found_time, message.victim_ids[0])
+            self.confirm_and_publish_victim(message.victim_ids[0])
+
+
+    def log_new_victim(self, victim_found_time, victim_id):
+        '''Logs new vicitm to console and logfile.'''
+        status_msg = "Time " + str((victim_found_time - self.autonomy_start_time).to_sec())
+        status_msg += " received victim found message, id = "
+        status_msg += victim_id
+        rospy.loginfo(status_msg)
+
+        row_to_write = [str((victim_found_time - self.autonomy_start_time).to_sec()), self.num_victims_found]
+        self._victim_file_writer.writerow(row_to_write)
+
+
+    def confirm_and_publish_victim(self, victim_id):
+        '''Confirms victim and publishes confirmation.'''
+        answer = VictimAnswer()
+        answer.task_id = victim_id
+        answer.answer = VictimAnswer.CONFIRM
+
+        self._publisher.publish(answer)
+
+        status_msg = "Confirmed victim with id: "
+        status_msg += victim_id
+        rospy.loginfo(status_msg)
+
+
+    def handle_occupancy_grid(self, msg):
+        '''Callback method for the occupancy grid.'''
+        self._occupancy_grid = msg
+
+
     def send_autonomy_start(self):
-                  # re-send data for mapping or sar mission
-            #if self._widget.sar_radiobutton.isChecked():
-            #    self._on_sar_radiobutton_pressed()
-            #else:
-            #    self._on_mapping_radiobutton_pressed()
-            
-            # add exploration task if not done so far
-            #if not self._initializedExploreTask:
-                req = AddTask._request_class()
-                req.task.state.state = TaskState.PENDING
+        '''Starts FlexBe exploration behavior and starts callback for logging
+        map exploration progress.
+        '''
+        self.start_exploration()
 
-                try:
-                    rospy.wait_for_service('taskallocation/add_task',3)
-                except rospy.exceptions.ROSException, e:
-                    err_msg = "error: "
-                    err_msg += str(e)
-                    #self._write_message(err_msg)
-                    rospy.logerr(err_msg)
-                    return
-                    
-                try:
-                    add_task = rospy.ServiceProxy('taskallocation/add_task', AddTask)
-                    
-                    req.task.header.stamp = rospy.Time(0)
-                    req.task.header.frame_id = "/map"
-                    req.task.pose.orientation.w = 1
-                    
-                    req.task.details.task_type = SarTaskTypes.START
-                    req.task.details.task_id = SarTaskTypes.START
-                    req.task.details.boolParams = []
-                    req.task.details.floatParams = []
-                    req.task.details.floatParams.insert(SarTaskTypes.INDEX_START_DIRECTION, 5.0)
-                    #print req
-                    add_task(req)
-
-                    req.task.details.task_type = SarTaskTypes.EXPLORE
-                    req.task.details.task_id = "explore"
-                    req.task.details.boolParams = []
-                    req.task.details.floatParams = []
-                    
-                    #if self._widget.sar_radiobutton.isChecked():
-                    req.task.details.boolParams.insert(0, True)
-                    #elif self._widget.mapping_radiobutton.isChecked():
-                    #    req.task.details.boolParams.insert(0, False)
-                    #else:
-                    #    print 'Warning: No mission selected!'
-
-                    #print req
-                    add_task(req)
-                    
-                except rospy.ServiceException, e:
-                    err_msg = "error: "
-                    err_msg += str(e)
-                    self._write_message(err_msg)
-                
-                
-                
-                rospy.loginfo("Sent start request")
-                self._autonomy_start_time = rospy.Time.now()
-                
-                self.add_map_data_entry()
-                
-                self._autonomyPublisher.publish(True)
-                self._autonomous = True
-                
-                self._map_timer = rospy.Timer(rospy.Duration(10), self.map_timer_callback)
-                #self._widget.autonomy_button.setText(self._stopAutonomyText)
-                #self._write_message('started autonomy')
-            
-                #self._widget.start_mode_combobox.setCurrentIndex(4)
-            
-                #self._on_add_test_object()
-  
-
-
-    # Velocity callback. Recomputes trajectory when new velocity command comes in
-    def vel_cmd_callback(self, data):
-        self._velocity_command = data.data
-        if self._velocity_command > 0:
-            self._trajectory_points = self.set_motion_properties(0, self._velocity_command, self._param_acceleration, self._param_min_angle, self._param_max_angle)
-        else:
-            self._trajectory_points = []
-        self._client.cancel_all_goals()
-
-    #def run(self):
-        #next_start_time = rospy.Time.now()
+        self.autonomy_start_time = rospy.Time.now()
+        self.write_map_exploration_progress_entry()
         
-     #   while not rospy.is_shutdown():
-     #     rospy.sleep(0.1)
-          
+        self._autonomy_publisher.publish(True)
+        self._autonomous = True
+        
+        self._map_timer = rospy.Timer(rospy.Duration(10), self.map_timer_callback)
+
+
+    def start_exploration(self):
+        '''Task that starts exploration flexbe behavior.'''
+        #TODO: not yet implemented.
+        rospy.loginfo("FlexBe exploration behavior activated.")
+
+
+    def map_timer_callback(self):
+        '''Callback for writing map exploration progress.'''
+        self.write_map_exploration_progress_entry()
+
+
+    def write_map_exploration_progress_entry(self):
+        '''Writes an entry with known cells and elapsed time in map-file.'''
+        num_cells = self.compute_known_cells()
+        elapsed_time_in_sec = str((rospy.Time.now() - self.autonomy_start_time).to_sec())
+      
+        self._map_file_writer.writerow([str(elapsed_time_in_sec), str(num_cells)])
+
+
+    def compute_known_cells(self):
+        '''Computes currently known cells in environment'''
+        if not self._occupancy_grid:
+            return 0
+
+        num_unknown_cells = 0.0
+
+        for cell in self._occupancy_grid.data:
+            if cell == -1:
+                num_unknown_cells += 1
+        
+        return len(self._occupancy_grid.data) - num_unknown_cells
+
+
+    def close_file_writers(self):
+        '''Closes open file writers.'''
+        self._map_file.close()
+        self._victim_file.close()
+
+
 if __name__ == "__main__":
     '''Creates an instance of ExplorationTester for a specified time. 
     Afterwards prints statistics and saves map.
@@ -251,17 +180,20 @@ if __name__ == "__main__":
 
     r = rospy.Rate(10) # 10hz
     
-    #script runtime: 600 sec sim-time.
-    while not (rospy.Time.now() - exploration_instance._autonomy_start_time).to_sec() > 60 * 10:
+    # 50 sec. sim-time.
+    while not (rospy.Time.now() - exploration_instance.autonomy_start_time).to_sec() > 5 * 10:
         r.sleep()
     
-    printAndSaveExplorationStatistics(exploration_instance)
+    print_and_save_exploration_statistics(exploration_instance)
     
+    exploration_instance.close_file_writers()
+
     rospy.sleep(10)
     rospy.loginfo("ExplorationTester done. Exiting.")
 
 
-def printAndSaveExplorationStatistics(exploration_instance):
+def print_and_save_exploration_statistics(exploration_instance):
+    '''Prints final statistics of exploration trial and saves map.'''
     rospy.loginfo("Experiment at " + exploration_instance.date_string + " ended.")
     rospy.loginfo("Robot found " + str(exploration_instance.num_victims_found) + " victims.")
         
@@ -269,4 +201,3 @@ def printAndSaveExplorationStatistics(exploration_instance):
     geotiff_string = String()
     geotiff_string = "savegeotiff"
     exploration_instance.sys_command_publisher.publish(geotiff_string)
-
