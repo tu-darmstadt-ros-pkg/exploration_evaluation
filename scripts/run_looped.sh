d08b712098cab71878592afeeeedfb7c1a214d09
Stefan Wegener <stefan@stefan-wegener.eu>
2016-08-19 12:26:02 +0200

    Modifications for new copy-script of geotiff-maps

--- scripts/run_looped.sh
+++ scripts/run_looped.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Bash script that launches exploration_evaluation either in an infinite
 # loop (if no optional parameter passed) or $1 times.
+#
+# Parameter:
+# $1: Number of times script gets called.
+# $2: Path to script that copies geotiff maps (and its parameters).
 COUNTER=1
 
 # run $1 times
@@ -11,6 +15,10 @@ if [ ! -z "$1" ]; then
         roscore&
         sleep 10
         roslaunch exploration_evaluation launch_eval_complete.launch
+        # Saving geotiff if $2 exists.
+        if [ ! -z "$2" ]; then
+            $2 $COUNTER
+        fi
         echo Ran $COUNTER experiments
         ((COUNTER+=1))
         sleep 10
